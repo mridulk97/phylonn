4914bb0edc880108aeefc37c274e17c16406c801
Mohannad Elhamod <elhamod@tinkercliffs1.cm.cluster>
2022-10-06 09:36:50 -0400

    nit

--- hyperp/hyperp_phylo-VQVAE.sh
+++ hyperp/hyperp_phylo-VQVAE.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=ml4science
 #SBATCH --partition=a100_normal_q
-#SBATCH --time=2-00:00:00 
+#SBATCH --time=0-16:00:00 
 #SBATCH --gres=gpu:1 
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=8
 #SBATCH -o ./SLURM/slurm-%j.out
@@ -15,9 +15,10 @@ module load gcc/8.2.0
 source activate taming3 
 
 #python -m 
-wandb agent mndhamod/Phylo-VQVAE/vm4v0kh3
+wandb agent mndhamod/Phylo-VQVAE/1r1ohelu
 
 # To create a sweep:
 # wandb sweep --project Phylo-VQVAE hyperp/hyperp_bayes_nested.yaml 
+wandb sweep --project Phylo-VQVAE hyperp/hyperp_bayes_phyloloss_nested.yaml
 
 exit;
