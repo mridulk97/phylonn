b488d264dc0051d7facbb81dc1204e1d575c54b6
Mridul Khurana <mridul@vt.edu>
2023-01-21 16:03:35 -0500

    removed duplicate loader and using the original loader

--- run_tinkercliff_sbatch.sh
+++ run_tinkercliff_sbatch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J VQGAN
-#SBATCH --account=ml4science
+#SBATCH --account=mabrownlab
 #SBATCH --partition=dgx_normal_q
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 # this requests 1 node, 1 core. 
-#SBATCH --time=0-15:00:00 # 10 minutes
+#SBATCH --time=2-00:00:00 # 10 minutes
 #SBATCH --gres=gpu:2
 
 module reset
@@ -27,13 +27,13 @@ which python
 # python main.py --name CW-VQGAN-transformer --postfix testing_merge_code_transformers --base configs/cw_transformer_latest_tinker_model.yaml -t True --gpus 0,
 
 ## CUB
-# python main.py --name CUB-VQGAN --postfix cub_n_embed_1024_epoch_467_onwards --base configs/cub_vqgan_256_test.yaml -t True --gpus 0,1,2,3,
+python main.py --name CUB-VQGAN --postfix cub_n_embed_1024_epoch_771_onwards --base configs/cub_vqgan_256_test.yaml -t True --gpus 0,1,
 
 # CUB transformer
 # python main.py --name CUB-VQGAN-transformer --postfix cub_n_embed_1024_transformer_test --base configs/cub_vqgan_transformer.yaml -t True --gpus 0,1,2,3,
 
 #CUB - CW
-python main.py --name CUB-MODELS --postfix cub_cw_module_n_embed_1024_epoch_467 --base configs/cub_cw_module_256.yaml -t True --gpus 0,1,
+# python main.py --name CUB-MODELS --postfix cub_cw_module_n_embed_1024_epoch_467 --base configs/cub_cw_module_256.yaml -t True --gpus 0,1,
 
 
 exit;
