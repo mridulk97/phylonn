472a2a537775014894f4ca0c55bbbeb624b483b4
Mridul Khurana <mridul@vt.edu>
2023-01-23 21:41:11 -0500

    added cub data annotations scripts

--- run_tinkercliff_sbatch.sh
+++ run_tinkercliff_sbatch.sh
@@ -3,8 +3,8 @@
 #SBATCH --account=mabrownlab
 #SBATCH --partition=dgx_normal_q
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 # this requests 1 node, 1 core. 
-#SBATCH --time=2-00:00:00 # 10 minutes
-#SBATCH --gres=gpu:2
+#SBATCH --time=1-00:00:00 # 10 minutes
+#SBATCH --gres=gpu:4
 
 module reset
 
@@ -26,8 +26,8 @@ which python
 # python main.py --name CW-VQGAN-transformer --postfix new-tinker-latest --base configs/cw_transformer_latest_tinker_model.yaml -t True --gpus 0,1,2,3,4
 # python main.py --name CW-VQGAN-transformer --postfix testing_merge_code_transformers --base configs/cw_transformer_latest_tinker_model.yaml -t True --gpus 0,
 
-## CUB
-python main.py --name CUB-VQGAN --postfix cub_n_embed_1024_epoch_771_onwards --base configs/cub_vqgan_256_test.yaml -t True --gpus 0,1,
+# ## CUB
+# python main.py --name CUB-VQGAN --postfix cub_n_embed_1024_epoch_771_onwards --base configs/cub_vqgan_256_test.yaml -t True --gpus 0,1,
 
 # CUB transformer
 # python main.py --name CUB-VQGAN-transformer --postfix cub_n_embed_1024_transformer_test --base configs/cub_vqgan_transformer.yaml -t True --gpus 0,1,2,3,
@@ -35,5 +35,10 @@ python main.py --name CUB-VQGAN --postfix cub_n_embed_1024_epoch_771_onwards --b
 #CUB - CW
 # python main.py --name CUB-MODELS --postfix cub_cw_module_n_embed_1024_epoch_467 --base configs/cub_cw_module_256.yaml -t True --gpus 0,1,
 
+## CUB - segmented
+# python main.py --name CUB-VQGAN --postfix cub_segmented --base configs/cub_vqgan_256_segmented.yaml -t True --gpus 0,1,2,3,4,
+
+## CUB - segmented transformer
+python main.py --name CUB-VQGAN-transformer --postfix cub_segmented_transformer --base configs/cub_vqgan_256_segmented_transformer.yaml -t True --gpus 0,1,2,3,
 
 exit;
---
+++ taming/data/image_squarer.py
@@ -0,0 +1,63 @@
+import os
+import PIL
+import glob
+import argparse
+import numpy as np
+from tqdm import tqdm
+from torchvision import transforms
+
+
+def MakeSquared(img, imageDimension=256):
+
+    img_H = img.size[0]
+    img_W = img.size[1]
+    smaller_dimension = 0 if img_H < img_W else 1
+    larger_dimension = 1 if img_H < img_W else 0
+    if (imageDimension != img_H or imageDimension != img_W):
+        new_smaller_dimension = int(imageDimension * img.size[smaller_dimension] / img.size[larger_dimension])
+        if smaller_dimension == 1:
+            img = transforms.functional.resize(img, (new_smaller_dimension, imageDimension))
+        else:
+            img = transforms.functional.resize(img, (imageDimension, new_smaller_dimension))
+
+        diff = imageDimension - new_smaller_dimension
+        pad_1 = int(diff/2)
+        pad_2 = diff - pad_1
+
+        # # imagenet mean
+        # mean = np.asarray([ 0.485, 0.456, 0.406 ])
+        # fill = tuple([int(round(mean[0]*255)), int(round(mean[1]*255)), int(round(mean[2]*255))])
+
+        fill = tuple([255, 255, 255])
+
+        if smaller_dimension == 0:
+            img = transforms.functional.pad(img, (pad_1, 0, pad_2, 0), padding_mode='constant', fill = fill)
+        else:
+            img = transforms.functional.pad(img, (0, pad_1, 0, pad_2), padding_mode='constant', fill = fill)
+
+    return img
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='List cub data files')
+    parser.add_argument('--image_dir', type=str, help='Path of images to be resized')
+    parser.add_argument('--output_dir', type=str, help='Path of resized images')
+
+    args = parser.parse_args()
+
+    for folder in tqdm(sorted(glob.glob(os.path.join(args.image_dir, '*')))):
+        species = folder.split('/')[-1]
+        new_folder = os.path.join(args.output_dir, species)
+        if not os.path.isdir(new_folder):
+            os.makedirs(new_folder)
+        
+        for image_path in sorted(glob.glob(folder+'/*')):
+            image_file = image_path.split('/')[-1]
+            image = PIL.Image.open(image_path)
+            try:
+                squared_image = MakeSquared(image)
+                new_image_path = os.path.join(new_folder, image_file)
+                squared_image.save(new_image_path)
+            except Exception as error:
+                print(image_path)
+                print(error)
