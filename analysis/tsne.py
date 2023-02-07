60684cd1a804c218700bbdcea71f3953b3f789a7
Mridul Khurana <mridul@vt.edu>
2023-02-07 16:00:51 -0500

    updated tsne code

--- analysis/tsne.py
+++ analysis/tsne.py
@@ -4,8 +4,9 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from taming.analysis_utils import get_phylomapper_from_config
 from taming.data.phylogeny import Phylogeny
-from taming.loading_utils import load_config, load_phylovqvae
+from taming.loading_utils import load_config, load_phylovqvae, load_CWVQGAN
 from taming.models.phyloautoencoder import PhyloVQVAE
+from taming.models.cwautoencoder import CWmodelVQGAN
 from taming.models.vqgan import VQModel
 from taming.plotting_utils import get_fig_pth
 from torchvision import transforms
@@ -23,6 +24,7 @@ from omegaconf import OmegaConf
 from taming.data.utils import custom_collate
 from taming.data.custom import CustomTest as CustomDataset
 from torch.utils.data import DataLoader
+from sklearn.neighbors import NearestNeighbors
 import taming.constants as CONSTANTS
 
 MAX_DIMS_PCA=100
@@ -34,11 +36,15 @@ def get_output(model, image):
     elif type(model) == VQModel: 
         quant, _, _ = model.encode(image)
         return quant
+    elif type(model) == CWmodelVQGAN:
+        # quant, _, _ = model.encode(image)
+        h = model.encoder(image)
+        h = model.quant_conv(h)
+        return h
     else:
         raise "Model type unknown"
     
     
-
 
 
 ##############
@@ -50,7 +56,8 @@ def get_tsne(dataloader, model, path,
     which_tsne_plots = ['standard', 'images', 'incorrect']
     , file_prefix='default_name'
     , img_res=None,
-    phylomapper = None):
+    phylomapper = None,
+    phylogeny_knn=None):
 
     # Go thtough batches
     X = None
@@ -84,7 +91,17 @@ def get_tsne(dataloader, model, path,
         plot_tsne_dots(dataloader, tx, ty, path, file_prefix, legend_labels, phylomapper)
     
     if 'incorrect' in which_tsne_plots:
-        plot_correct_incorrect(dataloader, tx, ty, path, file_prefix, model, cuda)
+        plot_correct_incorrect(dataloader, tx, ty, path, file_prefix, model, phylomapper, cuda)
+    
+    if ('knn' in which_tsne_plots):
+        if phylogeny_knn is None:
+            print('KNN plot cannot be calculated if phylogeny_knn is not passed! skipping...')
+        if phylomapper is not None:
+            print('KNN can only be calculated at level 3! skipping...')
+        else:
+            phylogeny_knn = Phylogeny(phylogeny_knn)
+            plot_phylo_KNN(dataloader, tx, ty, path, file_prefix, phylogeny_knn)
+            
 
     
 
@@ -94,6 +111,106 @@ def get_tsne(dataloader, model, path,
 
 
 
+
+
+
+
+
+def avg_distances(fine_label, indexes, phylogeny, dataset):
+    dist = 0
+    label_list = phylogeny.getLabelList()
+    for i in indexes:
+        lbl = dataset[i]['class']
+        dist = dist + phylogeny.get_distance(label_list[fine_label], label_list[lbl])
+        # print(fine_label, i, lbl, dist)
+    result = dist/len(indexes)
+    return result
+
+
+
+def plot_phylo_KNN(dataloader, tx, ty, path, file_prefix, phylogeny_knn, n_neighbors=5):
+    # Parse through the dataloader images
+    KNN_values = None
+
+    coord = np.hstack((np.array(tx).reshape(-1,1),np.array(ty).reshape(-1,1)))
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors).fit(coord)
+    _, indexes = nbrs.kneighbors(coord)
+    
+    acc=0
+    for i, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
+        fine_label = batch['class']
+        
+        KNN_values_ = torch.tensor([avg_distances(x, indexes[acc+i_], phylogeny_knn, dataloader.dataset) for i_, x in  enumerate(fine_label)]) 
+        KNN_values = KNN_values_ if KNN_values is None else torch.cat([KNN_values, KNN_values_]).detach()
+        
+        acc = acc + len(fine_label)
+
+
+    df = pd.DataFrame()
+    df['tsne-x'] = tx
+    df['tsne-y'] = ty
+    df['KNN_phylo_dist'] = KNN_values
+
+    matplotlib.pyplot.figure(figsize=(16,10))
+    sns_plot = sns.scatterplot(
+        x="tsne-x", y="tsne-y",
+        hue='KNN_phylo_dist',
+        palette="Oranges",
+        data=df,
+        legend="full"
+    )
+
+    norm = plt.Normalize(df['KNN_phylo_dist'].min(), df['KNN_phylo_dist'].max())
+    sm = plt.cm.ScalarMappable(cmap="Oranges", norm=norm)
+    sm.set_array([])
+
+    # Remove the legend and add a colorbar
+    sns_plot.get_legend().remove()
+    sns_plot.figure.colorbar(sm)
+
+    fig = sns_plot.get_figure()
+    save_path = os.path.join(path, file_prefix+"_tsne_KNN_phylo.png")
+    print(save_path)
+    fig.savefig(save_path, bbox_inches='tight',dpi=300)  
+
+
+def plot_correct_incorrect(dataloader, tx, ty, path, file_prefix, model=None, phylomapper=None, cuda=None):
+    # Parse through the dataloader images
+    equality = None
+
+    for i, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
+        fine_label = batch['class']
+        if phylomapper is not None:
+            fine_label = phylomapper.get_mapped_truth(fine_label)
+            
+        image2 = batch['image']
+        
+        if cuda is not None:
+            image2 = image2.cuda()
+            fine_label = fine_label.cuda()
+        
+        _, _, _, in_out_disentangler = model(image2)
+        pred_ = in_out_disentangler[CONSTANTS.DISENTANGLER_CLASS_OUTPUT]
+        _, pred_ = torch.max(torch.nn.Softmax(dim=1)(pred_),1)
+        equality_ = torch.eq(pred_, fine_label)
+        equality = equality_ if equality is None else torch.cat([equality, equality_]).detach()
+
+    df = pd.DataFrame()
+    df['tsne-x'] = tx
+    df['tsne-y'] = ty
+    fine_labels = equality.tolist()
+    df['isCorrect'] = fine_labels
+
+    matplotlib.pyplot.figure(figsize=(16,10))
+    sns_plot = sns.scatterplot(
+        x="tsne-x", y="tsne-y",
+        hue='isCorrect',
+        palette=sns.color_palette("hls", len(set(fine_labels))),
+        data=df,
+        legend="full"
+    )
+    fig = sns_plot.get_figure()
+    fig.savefig(os.path.join(path, file_prefix+"_tsne_correctPrediction.png"))
 
 
 def plot_tsne_dots(dataloader, tx, ty, path, file_prefix, legend_labels=[CONSTANTS.DISENTANGLER_CLASS_OUTPUT], phylomapper=None):
@@ -127,13 +244,13 @@ def plot_tsne_dots(dataloader, tx, ty, path, file_prefix, legend_labels=[CONSTAN
             hue=j,
             palette=sns.color_palette("hls", len(set(labels[j]))),
             data=df,
-            legend="full"
+            legend=False
         )
         # tooltip_label = [str(j) for i in range(len(labels))] #TODO: is this i a j?
         # tooltip = mpld3.plugins.PointLabelTooltip(sns_plot, labels=tooltip_label)
         fig = sns_plot.get_figure()
         # mpld3.plugins.connect(fig, tooltip)
-        fig.savefig(os.path.join(path, file_prefix+"_legend_" + j +"_tsne_dots.png"))
+        fig.savefig(os.path.join(path, file_prefix+"_legend_" + j +"_tsne_dots.png"), bbox_inches='tight',dpi=300)
 
 
 
@@ -188,7 +305,7 @@ def visualize_tsne_images(dataloader, tx, ty, img_res, path, file_prefix, phylom
     matplotlib.pyplot.figure(figsize = (16,12))
     # full_image = full_image.transpose(Image.FLIP_TOP_BOTTOM)
     imshow(full_image)
-    full_image.save(os.path.join(path, file_prefix+"_tsne_images.png")) 
+    full_image.save(os.path.join(path, file_prefix+"_tsne_images.png"))
     
 
 
@@ -209,6 +326,8 @@ def main(configs_yaml):
     batch_size = configs_yaml.batch_size
     num_workers = configs_yaml.num_workers
     
+    phylogeny_knn = configs_yaml.phylogeny_knn
+    
     isOriginalVQGAN = configs_yaml.isOriginalVQGAN if "isOriginalVQGAN" in configs_yaml.keys() else False
     
     phylogeny_path = configs_yaml.phylogeny_path
@@ -225,8 +344,10 @@ def main(configs_yaml):
     dataloader = DataLoader(dataset.data, batch_size=batch_size, num_workers=num_workers, collate_fn=custom_collate)
     
     # Load model
-    if isOriginalVQGAN:
+    if isOriginalVQGAN=='VQGAN':
         model_type=VQModel
+    elif isOriginalVQGAN=='CW':
+        model_type=CWmodelVQGAN
     else:
         model_type=PhyloVQVAE
         
@@ -241,6 +362,7 @@ def main(configs_yaml):
             , file_prefix=file_prefix
             , img_res=img_res,
             phylomapper=phylomapper,
+            phylogeny_knn=phylogeny_knn,
             cuda=DEVICE)
 
 
