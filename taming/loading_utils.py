bb666ae6ece2aec25f40c9515c4280886b1c68b8
Harish Babu Manogaran <harish7397@gmail.com>
2023-02-09 01:03:16 -0500

    Update loading_utils.py

--- taming/loading_utils.py
+++ taming/loading_utils.py
@@ -17,7 +17,6 @@ def load_config(config_path, display=False):
 def load_model(config, ckpt_path=None, data=None, cuda=False, model_type=PhyloVQVAE):
     model = model_type(**config.model.params)
     if ckpt_path is not None:
-        print('Loading model from', ckpt_path)
         sd = torch.load(ckpt_path, map_location="cpu")["state_dict"]
         missing, unexpected = model.load_state_dict(sd, strict=True)
     if cuda:
