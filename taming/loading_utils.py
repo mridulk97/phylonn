788f0789656f417b25a28eb4100d8e386d1cfe09
Harish Babu Manogaran <harish7397@gmail.com>
2023-02-09 00:58:14 -0500

    Update loading_utils.py

--- taming/loading_utils.py
+++ taming/loading_utils.py
@@ -6,7 +6,6 @@ from taming.models.LSFautoencoder import LSFVQVAE
 from omegaconf import OmegaConf
 import torch
 
-######### loaders
 
 def load_config(config_path, display=False):
     config = OmegaConf.load(config_path)
@@ -15,26 +14,7 @@ def load_config(config_path, display=False):
     return config
 
 
-#TODO: This should become one function for all models.
-def load_phylovqvae(config, ckpt_path=None, cuda=False, model_type=PhyloVQVAE):
-    model = model_type(**config.model.params)
-    if ckpt_path is not None:
-        sd = torch.load(ckpt_path, map_location="cpu")["state_dict"]
-        missing, unexpected = model.load_state_dict(sd, strict=True)
-    if cuda:
-        model = model.cuda()
-    return model.eval()
-
-def load_CWVQGAN(config, ckpt_path=None, data=None, cuda=False, model_type=CWmodelVQGAN):
-    model = model_type(**config.model.params)
-    if ckpt_path is not None:
-        sd = torch.load(ckpt_path, map_location="cpu")["state_dict"]
-        missing, unexpected = model.load_state_dict(sd, strict=True)
-    if cuda:
-        model = model.cuda()
-    return model.eval()
-
-def load_LSFvqvae(config, ckpt_path=None, data=None, cuda=False, model_type=LSFVQVAE):
+def load_model(config, ckpt_path=None, data=None, cuda=False, model_type=PhyloVQVAE):
     model = model_type(**config.model.params)
     if ckpt_path is not None:
         print('Loading model from', ckpt_path)
@@ -43,5 +23,3 @@ def load_LSFvqvae(config, ckpt_path=None, data=None, cuda=False, model_type=LSFV
     if cuda:
         model = model.cuda()
     return model.eval()
-
-
