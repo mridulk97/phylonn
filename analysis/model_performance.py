eb2f9842bbec89c99ad23925aad2cd52901abe06
Mridul Khurana <mridul@vt.edu>
2023-02-11 22:12:06 -0500

    fixed static paths for cosine distance files and figuures

--- analysis/model_performance.py
+++ analysis/model_performance.py
@@ -1,7 +1,9 @@
-from scripts.loading_utils import load_config, load_phylovqvae, load_CWVQGAN
+from scripts.loading_utils import load_config, load_model
 from scripts.data.custom import CustomTest as CustomDataset
 from scripts.data.utils import custom_collate
 from scripts.models.vqgan import VQModel
+from scripts.models.phyloautoencoder import PhyloVQVAE
+from scripts.models.cwautoencoder import CWmodelVQGAN
 
 from torch.utils.data import DataLoader
 from pytorch_lightning.trainer import Trainer
@@ -22,7 +24,7 @@ def main(configs_yaml):
     file_list_path= configs_yaml.file_list_path
     batch_size= configs_yaml.batch_size
     num_workers= configs_yaml.num_workers
-    load_model = configs_yaml.load_model
+    model_name = configs_yaml.load_model
 
     dataset = CustomDataset(size, file_list_path, add_labels=True)
     dataloader = DataLoader(dataset.data, batch_size=batch_size, num_workers=num_workers, collate_fn=custom_collate)
@@ -30,15 +32,17 @@ def main(configs_yaml):
     # Load model
     config = load_config(yaml_path, display=False)
 
-    if load_model=='CW':
+    if model_name=='CW':
         print('loading CW model')
-        model = load_CWVQGAN(config, ckpt_path=ckpt_path, data=dataset.data, cuda=(DEVICE is not None))
-    elif load_model=='VQMODEL':
+        model_type = CWmodelVQGAN
+    elif model_name=='VQMODEL':
         print('loading VQMODEL model')
-        model = load_phylovqvae(config, ckpt_path=ckpt_path, cuda=(DEVICE is not None), model_type=VQModel) 
+        model_type = VQModel
     else:
         print('loading Phlyo-NN')
-        model = load_phylovqvae(config, ckpt_path=ckpt_path, cuda=(DEVICE is not None))
+        model_type = PhyloVQVAE
+
+    model = load_model(config, ckpt_path=ckpt_path, cuda=(DEVICE is not None), model_type=model_type) 
     
     model.set_test_chkpt_path(ckpt_path)
 
